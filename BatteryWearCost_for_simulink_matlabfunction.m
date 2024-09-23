ac4337dc25ec4b10ecdbf8b99f5604b8f27b18b0
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-09-23 14:40:47 +0900

    Update BatteryWearCost_for_simulink_matlabfunction.m

--- BatteryWearCost_for_simulink_matlabfunction.m
+++ BatteryWearCost_for_simulink_matlabfunction.m
@@ -1,4 +1,4 @@
-function C_bess = BatteryDegradationCost(SOC_cur)
+function C_bess = BatteryDegradationCost(SOC_cur, BattCap)
     % MATLAB Function 블록에서는 모든 변수를 입력 및 출력으로 정의해야 합니다.
     % 따라서 phi_pre와 C_bess는 persistent로 정의하여 상태를 유지합니다.
     
@@ -22,14 +22,16 @@ function C_bess = BatteryDegradationCost(SOC_cur)
     % 누적된 C_bess 값을 출력
     C_bess = C_bess_accum;
     
-    % phi_pre를 업데이트
-    %phi_pre = IntegralWearDensityFunc(SOC);
+    % SOC_pre를 업데이트
     SOC_pre = SOC_cur;
+
     % Define function for Wear Density
     function w_s = WearDensityFunc(s)
         % Define parameters
-        C_bess_price = 3*10^5; % [$/MWh]
+        %C_bess_price = 3*10^5; % [$/MWh]
         %C_bess_price = 10000/16;
+        battPrice = 240000; %[$]
+        C_bess_price = battPrice / BattCap;
         eta_ch = 0.95; eta_dis = 0.95;
         A = 694; B = 0.795;
 
@@ -48,7 +50,9 @@ function C_bess = BatteryDegradationCost(SOC_cur)
 
     % Unit degradation cost calculation
     function C_bess_unit = UnitDegCost(SOC_cur, phi_pre)
-        E_cap = 0.8; % [MWh]
+        %E_cap = 0.8; % [MWh]
+        %E_cap = battEnergy;
+        E_cap = BattCap;
         phi_cur = IntegralWearDensityFunc(SOC_cur);
         C_bess_unit = E_cap * (phi_cur - phi_pre);
         
@@ -58,14 +62,12 @@ function C_bess = BatteryDegradationCost(SOC_cur)
 
     %% version 2 code: integral soct, soct-1
     function phi = IntegralWearDensityFunc2(SOC_pre, SOC_cur)
-        %SOC_init = 50 / 100; % Convert to fraction (0-1)
-        %SOC_cur = SOC_cur / 100;   % Convert to fraction (0-1)
-        fprintf("SOC_pre : %f\n", SOC_pre);
         % Trapezoidal numerical integration approximation
         phi = (WearDensityFunc(SOC_pre) + WearDensityFunc(SOC_cur)) * (SOC_cur - SOC_pre) / 2;
     end
     function C_bess_unit = UnitDegCost2(SOC_cur, SOC_pre)
-        E_cap = 0.8; %[MWh]
+        E_cap = BattCap; %[MWh]
+        %E_cap = 9.000000000000000e+09;
         C_bess_unit = E_cap* IntegralWearDensityFunc2(SOC_pre, SOC_cur);
         
         % Wear Cost is always positive
