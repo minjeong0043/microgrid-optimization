75f791e4151700e1d906fbbe5d6f36197d7d64ec
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-09-25 19:36:17 +0900

    Update battSolarOptimize_ver2.m

--- battSolarOptimize_ver2.m
+++ battSolarOptimize_ver2.m
@@ -1,31 +1,36 @@
-function [Pgrid,Pbatt,Ebatt, BattWearCost] = battSolarOptimize_ver1(N,dt,Ppv,Pload,Einit,Cost,FinalWeight,batteryMinMax)
+function [Pgrid,Pbatt,Ebatt] = battSolarOptimize_ver2(N,dt,Ppv,Pload,Einit,Cost,FinalWeight,batteryMinMax, c, d)
 
 % Minimize the cost of power from the grid while meeting load with power 
 % from PV, battery and grid 
-
+% fprintf("c = %f, ", c)
+% fprintf("d = %f\n", d)
 prob = optimproblem;
 
 % Decision variables
-PgridV = optimvar('PgridV',N, 'LowerBound',0);
+PgridV = optimvar('PgridV',N);
 PbattV = optimvar('PbattV',N,'LowerBound',batteryMinMax.Pmin,'UpperBound',batteryMinMax.Pmax);
 EbattV = optimvar('EbattV',N,'LowerBound',batteryMinMax.Emin,'UpperBound',batteryMinMax.Emax);
-BattWearCostV = optimvar('BattWearCostV', N, 'LowerBound', 0);
-
+L = optimvar('L', N); %  하한 설정해서 값 안 나오면 하한 설정 지워보기.
+Q = optimvar('Q', N, 'LowerBound',0);
 % Minimize cost of electricity from the grid
 prob.ObjectiveSense = 'minimize';
-prob.Objective = dt * Cost' * PgridV - FinalWeight * EbattV(N) + BattWearCostV(N);
+prob.Objective = dt*Cost'*PgridV - FinalWeight*EbattV(N) + (L(N) - L(N-1));
 
 % Power input/output to battery
 prob.Constraints.energyBalance = optimconstr(N);
 prob.Constraints.energyBalance(1) = EbattV(1) == Einit;
 prob.Constraints.energyBalance(2:N) = EbattV(2:N) == EbattV(1:N-1) - PbattV(1:N-1)*dt;
 
-% battery Wear Cost input/output
-E_cap = 2500;
-prob.Constraints.battDeg = optimconstr(N);
-prob.Constraints.battDeg(1) = BattWearCostV(1) == 0;
-prob.Constraints.battDeg(2:N) = BattWearCostV(2:N) == BattWearCostV(1:N-1) + E_cap * (phi(EbattV(2:N)) - phi(EbattV(1:N-1)));
+% Define Lfunc
+prob.Constraints.L = optimconstr(N);
+prob.Constraints.L(1:N) = L(1:N) == 1/2 * (Q(1:N)).^2;
 
+% Define Q
+Cd = 2400000 / 2500; % [$/KWh]
+prob.Constraints.Qfunc = optimconstr(N);
+prob.Constraints.Qfunc(1) = Q(1) == 0;
+% prob.Constraints.Qfunc(2:N) = Q(2:N) == max(Q(1:N-1) - Cd*(PgridV(1:N-1)+Pload(1:N-1)), 0);
+prob.Constraints.Qfunc(2:N) = Q(2:N) == Q(1:N-1) - Cd*(PgridV(1:N-1)+Pload(1:N-1));
 % Satisfy power load with power from PV, grid and battery
 prob.Constraints.loadBalance = Ppv + PgridV + PbattV == Pload;
 
@@ -33,49 +38,20 @@ prob.Constraints.loadBalance = Ppv + PgridV + PbattV == Pload;
 x0.PgridV = zeros(N,1);
 x0.PbattV = zeros(N,1);
 x0.EbattV = Einit * ones(N,1);
-x0.BattWearCostV = zeros(N, 1);
+x0.L = zeros(N,1);
+x0.Q = zeros(N,1);
 
 % Solve the linear program
-%options = optimoptions(prob.optimoptions,'Display','none');
-% options = optimoptions('fmincon', 'Algorithm', 'interior-point','Display','none');
-options = optimoptions('fmincon', 'Algorithm', 'sqp','Display','none');
-[values,~,exitflag] = solve(prob,x0,'Options',options);
+options = optimoptions(prob.optimoptions,'Display','none');
+[values,~,exitflag] = solve(prob, x0,'Options',options);
 
-disp("PbattV")
-% Parse optimization results
+% Parse optmization results
 if exitflag <= 0
-    % 최적해 없거나 오류는 0을 반환함
     Pgrid = zeros(N,1);
     Pbatt = zeros(N,1);
     Ebatt = zeros(N,1);
-    BattWearCost = zeros(N,1);
 else
-    % 최적해 찾았을 때 값을 넣어줌
     Pgrid = values.PgridV;
     Pbatt = values.PbattV;
     Ebatt = values.EbattV;
-    BattWearCost = values.BattWearCostV;
-end
-end
-function w_s = WearDensityFunc(s)
-    % Define parameters
-    %C_bess_price = 3*10^5; % [$/MWh]
-    %C_bess_price = 10000/16;
-    BattCap = 2500;
-    battPrice = 240000; %[$]
-    C_bess_price = battPrice / BattCap;
-    eta_ch = 0.95; eta_dis = 0.95;
-    A = 694; B = 0.795;
-
-    % Calculate Wear Density func w(s)
-    w_s = (C_bess_price / (2 * eta_ch * eta_dis)) * (B * (1 - s)^(B - 1)) / A;
-end
-function phi = phi(EbattV)
-    battEnergy = 9*10^9;
-    SOC_init = 0.5;
-    SOC_cur = EbattV / battEnergy;
-    N = 241;
-    for t = 2:N-1
-        phi = (WearDensityFunc(SOC_init) + WearDensityFunc(SOC_cur(t))) * (SOC_cur(t) - SOC_init) / 2;
-    end
 end
